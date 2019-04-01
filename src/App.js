3ccbc19e00dde1871c02481defe78121ab580dcd
Tomi Karikj <karikjtomi@gmail.com>
2019-04-01 21:23:27 +0200

    Added new auth related routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import M from 'materialize-css';
 
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 import 'materialize-css/dist/css/materialize.min.css';
 
 class App extends Component {
+  componentDidMount() {
+    // Auto initialize all the things!
+    M.AutoInit();
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -16,6 +24,8 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Dashboard} />
             <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
