255f26fca0e059b23d790d7374c98045c8a6966d
Tomi Karikj <karikjtomi@gmail.com>
2019-04-02 15:44:46 +0200

    Started working on the project actions and reducer

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,6 +1,29 @@
-const initialState = {};
+const initialState = {
+  projects: [
+    {
+      id: '1',
+      title: 'sunt aut facere repellat provident',
+      content: 'quia et suscipit suscipit recusandae consequuntur expedita'
+    },
+    {
+      id: '2',
+      title: 'qui est esse',
+      content: 'est rerum tempore vitae sequi sint nihil reprehenderit'
+    },
+    {
+      id: '3',
+      title: 'ea molestias quasi exercitationem',
+      content: 'et iusto sed quo iure voluptatem occaecati'
+    }
+  ]
+};
 
 const projectReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT': {
+      console.log('project created successfully', action.project);
+    }
+  }
   return state;
 };
 
