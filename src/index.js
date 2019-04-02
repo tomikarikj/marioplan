fa4122f8782b9ee515cde4338a22c22676f63fe7
Tomi Karikj <karikjtomi@gmail.com>
2019-04-02 20:15:20 +0200

    Added firebase and thunk related logic

--- src/index.js
+++ src/index.js
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
 
-const store = createStore(rootReducer);
+import fbConfig from './config/fbConfig';
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(
+      thunk.withExtraArgument({
+        getFirebase,
+        getFirestore
+      })
+    ),
+    reduxFirestore(fbConfig),
+    reactReduxFirebase(fbConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
