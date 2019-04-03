812053dcb1bb6d5bc3d4496ecab8eb94ded231a6
Tomi Karikj <karikjtomi@gmail.com>
2019-04-03 22:24:28 +0200

    Added some firebase and firestore related helpers

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ const store = createStore(
       })
     ),
     reduxFirestore(fbConfig),
-    reactReduxFirebase(fbConfig, { attachAuthIsReady: true })
+    reactReduxFirebase(fbConfig, {
+      useFirestoreForProfile: true,
+      userProfile: 'users',
+      attachAuthIsReady: true
+    })
   )
 );
 
