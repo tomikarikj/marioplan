89d73d571604bdedea25bc125b9674ca24889a88
Tomi Karikj <karikjtomi@gmail.com>
2019-04-05 15:38:45 +0200

    Refactored the code to be dynamic

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,13 +2,15 @@ export const createProject = project => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     // make async call to db
     const firestore = getFirestore();
+    const profile = getState().firebase.profile;
+    const authorId = getState().firebase.auth.uid;
     firestore
       .collection('projects')
       .add({
         ...project,
-        authorFirstName: 'Tomi',
-        authorLastName: 'Karikj',
-        authorId: 12345,
+        authorFirstName: profile.firstName,
+        authorLastName: profile.lastName,
+        authorId,
         createdAt: new Date()
       })
       .then(() => {
