f36181fe91d26b8394a809cc4a8129652fd63534
Tomi Karikj <karikjtomi@gmail.com>
2019-04-05 15:38:20 +0200

    Added a redirect after creating a project

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -19,6 +19,7 @@ class CreateProject extends Component {
   handleSubmit = e => {
     e.preventDefault();
     this.props.createProject(this.state);
+    this.props.history.push('/');
   };
 
   render() {
